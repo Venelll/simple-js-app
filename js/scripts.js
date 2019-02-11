d5adc040dcf16ce3b1b6d371da6df6465eb88757
sahendrix36579 <sahendrix36579@gmail.com>
2019-02-11 15:52:53 -0700

    Fixed so the loop actually works.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
-var repository = {
-  name: "Bulbasaur",
-  height: 7,
-  typing: ['Grass', ' Poison'],
-  pokedexNumber: 1,
-}
+var repository = [
+  {name: 'Bulbasaur', height: 7,typing: ['Grass', ' Poison'], pokedexNumber: 1},
+  {name: 'Ivysaur', height: .9, typing: ['Grass', ' Poison'], pokedexNumber: 2},
+  {name: 'Venasaur', height: 2, typing: ['Grass', ' Poison'], pokedexNumber: 3},
+  {name: 'Arceus', height: 3.2, typing: ['Normal'], pokedexNumber:  493},
+]
 
-for (var i = 0; i >= 0; i--) {
-  document.write(repository.name,"<br>", ' Height: ', repository.height,
-  "<br>",'Type: ', repository.typing,
-  "<br>",'Pokedex Number: ', repository.pokedexNumber)
-    if (repository.pokedexNumber <= 9 ){
-      document.write (' - One of the first few mons to be added to the pokedex.');
+
+for (var i = 0; i < repository.length; i++) {
+  document.write("Name: ", repository[i].name, "<br>", "Height: ", repository[i].height," meters", "<br>", "Typing: ",
+                 repository[i].typing, "<br>", "Pokedex Number: ", repository[i].pokedexNumber, "<p>")
+
+    if (repository[i].pokedexNumber > 480){
+    document.write (' - This is a legendary pokemon')
     }
 }
