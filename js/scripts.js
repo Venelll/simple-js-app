976834b3379e78544ab3f2b4dd739466ee2adf3b
sahendrix36579 <sahendrix36579@gmail.com>
2019-02-12 11:42:15 -0700

    Fixed minor errors in code layout.

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,12 @@ var repository = [
 
 
 for (var i = 0; i < repository.length; i++) {
-  document.write("Name: ", repository[i].name, "<br>", "Height: ", repository[i].height," meters", "<br>", "Typing: ",
-                 repository[i].typing, "<br>", "Pokedex Number: ", repository[i].pokedexNumber, "<p>")
+  document.write("<p>")
+  document.write("<br>","Name: ", repository[i].name, "<br>", "Height: ", repository[i].height," meters", "<br>", "Typing: ",
+                 repository[i].typing, "<br>", "Pokedex Number: ", repository[i].pokedexNumber)
 
     if (repository[i].pokedexNumber > 480){
     document.write (' - This is a legendary pokemon')
     }
+    document.write("</p>")
 }
