89451a4b1477b34d2539df68dc92a917d5199376
sahendrix36579 <sahendrix36579@gmail.com>
2019-02-13 12:34:11 -0700

    Tried and failed at working on IIFE and modifying the script.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,48 @@
-var repository = [
-  {name: 'Bulbasaur', height: 7,typing: ['Grass', ' Poison'], pokedexNumber: 1},
-  {name: 'Ivysaur', height: .9, typing: ['Grass', ' Poison'], pokedexNumber: 2},
-  {name: 'Venasaur', height: 2, typing: ['Grass', ' Poison'], pokedexNumber: 3},
-  {name: 'Arceus', height: 3.2, typing: ['Normal'], pokedexNumber:  493},
-]
+//Original pokemon code //
+(function() {
+  var repository = [
+    {name: 'Bulbasaur', height: 7,typing: ['Grass', ' Poison'], pokedexNumber: 1},
+    {name: 'Ivysaur', height: .9, typing: ['Grass', ' Poison'], pokedexNumber: 2},
+    {name: 'Venasaur', height: 2, typing: ['Grass', ' Poison'], pokedexNumber: 3},
+    {name: 'Arceus', height: 3.2, typing: ['Normal'], pokedexNumber:  493},
+  ]
+  var pkmRepository = repository.slice();
 
+  for (var i = 0; i < repository.length; i++) {
+    document.write("<p>")
+    document.write("<br>","Name: ", repository[i].name, "<br>", "Height: ", repository[i].height," meters", "<br>", "Typing: ",
+    repository[i].typing, "<br>", "Pokedex Number: ", repository[i].pokedexNumber)
 
-for (var i = 0; i < repository.length; i++) {
+    if (repository[i].pokedexNumber > 480){
+      document.write (' - This is a legendary pokemon')
+    }
+    document.write("</p>")
+  }
+})
+
+//New pokemon code//
+var pkmRepository = (function () {
+  var pokemonRepository = [ //new repository
+    {name: 'Bulbasaur', height: 7,typing: ['Grass', ' Poison'], pokedexNumber: 1},
+    {name: 'Ivysaur', height: .9, typing: ['Grass', ' Poison'], pokedexNumber: 2},
+    {name: 'Venasaur', height: 2, typing: ['Grass', ' Poison'], pokedexNumber: 3},
+    {name: 'Arceus', height: 3.2, typing: ['Normal'], pokedexNumber:  493},
+  ];
+  function add(pokemon) {
+    pokemonRepository.add(item),
+  }
+  function getAll() {
+    return pokemonRepository,
+  }
+  return {
+    add: add,
+    getAll: getAll,
+  }
+});
+//I have absolutely zero idea what I am doing wrong.
+Object.keys(pkmRepository.pokemonRepository).forEach(function(property) {
   document.write("<p>")
-  document.write("<br>","Name: ", repository[i].name, "<br>", "Height: ", repository[i].height," meters", "<br>", "Typing: ",
+  document.write("<br>" + repository[i].name, "<br>", "Height: ", repository[i].height," meters", "<br>", "Typing: ",
   repository[i].typing, "<br>", "Pokedex Number: ", repository[i].pokedexNumber)
 
   if (repository[i].pokedexNumber > 480){
