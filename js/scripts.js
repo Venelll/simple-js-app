670a1c68b419e2e05bbf336ea96ba98f3612a422
sahendrix36579 <sahendrix36579@gmail.com>
2019-02-14 11:28:33 -0700

    Included better description of what does what and modified first private function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,6 @@
 //Original pokemon code //
 (function() {
-  var repository = [
-    {name: 'Bulbasaur', height: 7,typing: ['Grass', ' Poison'], pokedexNumber: 1},
-    {name: 'Ivysaur', height: .9, typing: ['Grass', ' Poison'], pokedexNumber: 2},
-    {name: 'Venasaur', height: 2, typing: ['Grass', ' Poison'], pokedexNumber: 3},
-    {name: 'Arceus', height: 3.2, typing: ['Normal'], pokedexNumber:  493},
-  ]
-  var pkmRepository = repository.slice();
+  var repository = repository.slice();
 
   for (var i = 0; i < repository.length; i++) {
     document.write("<p>")
@@ -18,35 +12,32 @@
     }
     document.write("</p>")
   }
-})
+});
 
 //New pokemon code//
 var pkmRepository = (function () {
-  var pokemonRepository = [ //new repository
+  //new repository
+  var pokemonRepository = [
     {name: 'Bulbasaur', height: 7,typing: ['Grass', ' Poison'], pokedexNumber: 1},
     {name: 'Ivysaur', height: .9, typing: ['Grass', ' Poison'], pokedexNumber: 2},
     {name: 'Venasaur', height: 2, typing: ['Grass', ' Poison'], pokedexNumber: 3},
     {name: 'Arceus', height: 3.2, typing: ['Normal'], pokedexNumber:  493},
   ];
-  function add(pokemon) {
-    pokemonRepository.add(item),
-  }
-  function getAll() {
-    return pokemonRepository,
-  }
+    //Add pokemon
+    function add (pokemon) {
+      pokemonRepository.push(pokemon);
+    };
+    //Makes pokemonRepository public
+    function  getAll () {
+      return pokemonRepository;
+    };
   return {
     add: add,
     getAll: getAll,
-  }
-});
-//I have absolutely zero idea what I am doing wrong.
-Object.keys(pkmRepository.pokemonRepository).forEach(function(property) {
-  document.write("<p>")
-  document.write("<br>" + repository[i].name, "<br>", "Height: ", repository[i].height," meters", "<br>", "Typing: ",
-  repository[i].typing, "<br>", "Pokedex Number: ", repository[i].pokedexNumber)
-
-  if (repository[i].pokedexNumber > 480){
-    document.write (' - This is a legendary pokemon')
-  }
-  document.write("</p>")
-}
+  };
+})();
+//I use pkmRepository.getAll() to retrieve the data in the array and then it should be printing out
+//what I want it to be, however it's not. Unless I messed up somewhere inside the IIFE, again.
+pkmRepository.getAll().forEach(function(name){
+                      document.write("<p>" + name + "<br>");
+                      });
