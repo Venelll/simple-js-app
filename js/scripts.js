6c9974f79ac6bc2f84ff675096d2ca190815e86c
sahendrix36579 <sahendrix36579@gmail.com>
2019-02-16 12:53:15 -0700

    Added an ability to remove pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-//New pokemon code//
+//New pokemon code
 var pkmRepository = (function () {
   //new repository
   var pokemonRepository = [
-    {name: 'Bulbasaur', height: 7,typing: ['Grass', ' Poison'], pokedexNumber: 1},
+    {name: 'Bulbasaur', height: .7,typing: ['Grass', ' Poison'], pokedexNumber: 1},
     {name: 'Ivysaur', height: .9, typing: ['Grass', ' Poison'], pokedexNumber: 2},
     {name: 'Venasaur', height: 2, typing: ['Grass', ' Poison'], pokedexNumber: 3},
     {name: 'Arceus', height: 3.2, typing: ['Normal'], pokedexNumber:  493},
@@ -11,14 +11,15 @@ var pkmRepository = (function () {
   function add (pokemon) {
     pokemonRepository.push(pokemon);
   };
-  //Makes pokemonRepository public
+  //Retrieves entire array.
   function  getAll () {
     return pokemonRepository;
   };
-  //removes a certain piece of the array
+  //removes a certain piece of the array.
   function remove () {
-    pokemonRepository.splice();
-  }
+    pokemonRepository.splice(i,1);
+  };
+  //Allows public usage of array and ability to manipulate array.
   return {
     add: add,
     getAll: getAll,
@@ -29,9 +30,9 @@ var pkmRepository = (function () {
 pkmRepository.getAll().forEach(function(currentItem){
   document.write("<p>" + currentItem.name + "<br>" + "Height: " + currentItem.height +" Meters" +
   "<br>" + "Typing: " + currentItem.typing + "<br>" +"Pokedex Number: " + currentItem.pokedexNumber);
-
+  //Checks legendary status.
   if (currentItem.pokedexNumber > 480){
-    document.write (" - This is a legendary pokemon")
-  }
-  document.write("</p>")
+    document.write (" - This is a legendary pokemon");
+  };
+  document.write("</p>");
 });
