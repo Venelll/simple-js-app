6ee36dfc4cdf25694dbabc8703a160f04c06d3c6
sahendrix36579 <sahendrix36579@gmail.com>
2019-02-18 13:34:27 -0700

    Changed if(..) - it now checks the legendary status of the pokemon rather than their pokedex number.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@
 var pkmRepository = (function () {
   //repository
   var pokemonRepository = [
-    {name: 'Bulbasaur', height: .7,typing: ['Grass', ' Poison'], pokedexNumber: 1},
-    {name: 'Ivysaur', height: .9, typing: ['Grass', ' Poison'], pokedexNumber: 2},
-    {name: 'Venasaur', height: 2, typing: ['Grass', ' Poison'], pokedexNumber: 3},
-    {name: 'Arceus', height: 3.2, typing: ['Normal'], pokedexNumber:  493},
+    {name: 'Bulbasaur', height: .7, typing: ['Grass', ' Poison'], pokedexNumber: 1, legendary: "no"},
+    {name: 'Ivysaur', height: .9, typing: ['Grass', ' Poison'], pokedexNumber: 2, legendary: "no"},
+    {name: 'Venasaur', height: 2, typing: ['Grass', ' Poison'], pokedexNumber: 3, legendary: "no"},
+    {name: 'Arceus', height: 3.2, typing: ['Normal'], pokedexNumber:  493, legendary: "yes"},
   ];
   //Add pokemon
   function add (pokemon) {
@@ -26,12 +26,12 @@ var pkmRepository = (function () {
     remove: remove,
   };
 })();
-//The new table of pokemon.
+//List of pokemon.
 pkmRepository.getAll().forEach(function(currentItem){
   document.write("<p>" + currentItem.name + "<br>" + "Height: " + currentItem.height +" Meters" +
   "<br>" + "Typing: " + currentItem.typing + "<br>" +"Pokedex Number: " + currentItem.pokedexNumber);
   //Checks legendary status.
-  if (currentItem.pokedexNumber > 480){
+  if (currentItem.legendary === "yes"){
     document.write (" - This is a legendary pokemon");
   };
   document.write("</p>");
