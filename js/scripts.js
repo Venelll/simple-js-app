40c863e7e0e2a458bb4eec68ad5da7e66180db2d
sahendrix36579 <sahendrix36579@gmail.com>
2019-02-28 21:07:01 -0700

    Created Modal to display window of pokemon info into JS.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,41 @@ var pokemonRepository = (function () {
   //repository
   var repository = [];
   var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=809';
+
+  // Elements
+  var modal = document.getElementById('custom-modal-ref')
+  var modalCloseButton = document.getElementById('custom-modal-close-ref')
+  var modalPaper = document.getElementById('custom-modal-paper-ref')
+
+  function handleClickOutsideModal(event) {
+    if (!modalPaper.contains(event.target)) {
+      hideModal()
+    }
+  }
+
+  function hideModal() {
+    modal.classList.add('hidden')
+  }
+
+  function handleKeyDown (event) {
+    if (!modal.classList.contains('hidden')) {
+      event = event || window.event;
+      var isEscape = false;
+      if ('key' in event) {
+        isEscape = (event.key === 'Escape' || event.key === 'Esc');
+      } else {
+        isEscape = (event.keyCode === 27);
+      }
+      if (isEscape) {
+        hideModal();
+      }
+    }
+  }
+
+  document.addEventListener('keydown', handleKeyDown)
+  modal.addEventListener('click', handleClickOutsideModal)
+  modalCloseButton.addEventListener('click', hideModal)
+
   //Add pokemon
   function add (pokemon) {
     repository.push(pokemon);
@@ -17,16 +52,30 @@ var pokemonRepository = (function () {
   };
   //shows detailed info about pokemon.
   function showDetails (pokemon) {
+
+    /**
+     * Show modal
+     */
     pokemonRepository.loadDetails(pokemon).then(function() {
-      console.log(pokemon);
+      modal.classList.remove('hidden');
+      modal.querySelector('h1').innerHTML = pokemon.name
+      modal.querySelector('p').innerHTML = 'Height: ' + pokemon.height
+      modal.querySelector('img').src = pokemon.imageUrl
     });
+
   };
   //Displays pokemon and comes with HTML formatting.
   function addListItem (pokemon) {
     var listNode = document.createElement('li');
     var buttonNode = document.createElement('button');
+
     buttonNode.innerHTML = pokemon.name;
     buttonNode.classList.add('pokemon');
+
+    buttonNode.addEventListener('click', function() {
+
+    })
+
     listNode.appendChild(buttonNode);
     document.getElementById('pokedexList').appendChild(listNode);
     //logs all the info about the pokemon.
